ef212e9ae094d7de43b7471dbd67625f1a127d7c
butyukunn82 <butyukunn82@gmail.com>
2026-09-13 14:18:53 +0900

    feat: mount interactive CVP break-even lab

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import ProcessCostLab from './features/ProcessCostLab'
 import VarianceLab from './features/VarianceLab'
 import OverheadVarianceLab from './features/OverheadVarianceLab'
 import DirectCostingLab from './features/DirectCostingLab'
+import CvpLab from './features/CvpLab'
 import './styles/app.css'
 import './styles/flow-fix.css'
 import './styles/process-lab.css'
 import './styles/variance-lab.css'
 import './styles/overhead-lab.css'
 import './styles/direct-costing-lab.css'
+import './styles/cvp-lab.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -20,6 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <VarianceLab />
       <OverheadVarianceLab />
       <DirectCostingLab />
+      <CvpLab />
     </div>
   </React.StrictMode>,
 )
