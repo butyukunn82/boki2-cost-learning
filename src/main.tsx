8b8c77f8f6a5e89127803abb8bb3fbd2d1590fb9
butyukunn82 <butyukunn82@gmail.com>
2026-09-13 14:32:22 +0900

    feat: mount 30-pattern industrial bookkeeping trainer

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import OverheadVarianceLab from './features/OverheadVarianceLab'
 import DirectCostingLab from './features/DirectCostingLab'
 import CvpLab from './features/CvpLab'
 import FirstMoveTrainer from './features/FirstMoveTrainer'
+import CorePatternTrainer from './features/CorePatternTrainer'
 import './styles/app.css'
 import './styles/flow-fix.css'
 import './styles/process-lab.css'
@@ -15,6 +16,7 @@ import './styles/overhead-lab.css'
 import './styles/direct-costing-lab.css'
 import './styles/cvp-lab.css'
 import './styles/first-move-trainer.css'
+import './styles/core-pattern-trainer.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -26,6 +28,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <DirectCostingLab />
       <CvpLab />
       <FirstMoveTrainer />
+      <CorePatternTrainer />
     </div>
   </React.StrictMode>,
 )
