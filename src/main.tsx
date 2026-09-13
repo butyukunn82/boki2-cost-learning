6e5361f08a46fcb15fb622c4f68450d6b01a3edc
butyukunn82 <butyukunn82@gmail.com>
2026-09-13 14:33:28 +0900

    feat: mount weighted-average versus FIFO process costing lab

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import DirectCostingLab from './features/DirectCostingLab'
 import CvpLab from './features/CvpLab'
 import FirstMoveTrainer from './features/FirstMoveTrainer'
 import CorePatternTrainer from './features/CorePatternTrainer'
+import ProcessMethodLab from './features/ProcessMethodLab'
 import './styles/app.css'
 import './styles/flow-fix.css'
 import './styles/process-lab.css'
@@ -17,12 +18,14 @@ import './styles/direct-costing-lab.css'
 import './styles/cvp-lab.css'
 import './styles/first-move-trainer.css'
 import './styles/core-pattern-trainer.css'
+import './styles/process-method-lab.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <div className="standalone-lab">
       <ProcessCostLab />
+      <ProcessMethodLab />
       <VarianceLab />
       <OverheadVarianceLab />
       <DirectCostingLab />
