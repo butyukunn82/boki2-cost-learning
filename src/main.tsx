ec6e403471829e22d15cd012e65f38d90b21d437
butyukunn82 <butyukunn82@gmail.com>
2026-09-13 14:04:39 +0900

    feat: mount process costing visual lab

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ProcessCostLab from './features/ProcessCostLab'
 import './styles/app.css'
 import './styles/flow-fix.css'
+import './styles/process-lab.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <div className="standalone-lab">
+      <ProcessCostLab />
+    </div>
   </React.StrictMode>,
 )
