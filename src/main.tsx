ed96a613c91c83b703b4e011caa123b9f6ab1aa8
butyukunn82 <butyukunn82@gmail.com>
2026-09-13 14:34:49 +0900

    feat: mount CBT practice shell

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import CvpLab from './features/CvpLab'
 import FirstMoveTrainer from './features/FirstMoveTrainer'
 import CorePatternTrainer from './features/CorePatternTrainer'
 import ProcessMethodLab from './features/ProcessMethodLab'
+import CbtPractice from './features/CbtPractice'
 import './styles/app.css'
 import './styles/flow-fix.css'
 import './styles/process-lab.css'
@@ -19,6 +20,7 @@ import './styles/cvp-lab.css'
 import './styles/first-move-trainer.css'
 import './styles/core-pattern-trainer.css'
 import './styles/process-method-lab.css'
+import './styles/cbt-practice.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -32,6 +34,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <CvpLab />
       <FirstMoveTrainer />
       <CorePatternTrainer />
+      <CbtPractice />
     </div>
   </React.StrictMode>,
 )
