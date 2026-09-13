c1f60d774372660fc32d43da29210affe7ccc1d2
butyukunn82 <butyukunn82@gmail.com>
2026-09-13 14:16:32 +0900

    feat: mount manufacturing overhead variance lab

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import ProcessCostLab from './features/ProcessCostLab'
 import VarianceLab from './features/VarianceLab'
+import OverheadVarianceLab from './features/OverheadVarianceLab'
 import './styles/app.css'
 import './styles/flow-fix.css'
 import './styles/process-lab.css'
 import './styles/variance-lab.css'
+import './styles/overhead-lab.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -14,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <div className="standalone-lab">
       <ProcessCostLab />
       <VarianceLab />
+      <OverheadVarianceLab />
     </div>
   </React.StrictMode>,
 )
