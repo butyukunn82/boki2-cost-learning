5f80a3852f177d1a09c090fe23e9e629d034b1ce
butyukunn82 <butyukunn82@gmail.com>
2026-09-13 14:37:32 +0900

    refactor: organize app into four learning modes

--- src/main.tsx
+++ src/main.tsx
@@ -1,43 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import ProcessCostLab from './features/ProcessCostLab'
-import VarianceLab from './features/VarianceLab'
-import OverheadVarianceLab from './features/OverheadVarianceLab'
-import DirectCostingLab from './features/DirectCostingLab'
-import CvpLab from './features/CvpLab'
-import FirstMoveTrainer from './features/FirstMoveTrainer'
-import CorePatternTrainer from './features/CorePatternTrainer'
-import ProcessMethodLab from './features/ProcessMethodLab'
-import DepartmentAllocationLab from './features/DepartmentAllocationLab'
-import CbtPractice from './features/CbtPractice'
+import LearningHub from './features/LearningHub'
 import './styles/app.css'
 import './styles/flow-fix.css'
 import './styles/process-lab.css'
 import './styles/process-method-lab.css'
+import './styles/department-lab.css'
 import './styles/variance-lab.css'
 import './styles/overhead-lab.css'
 import './styles/direct-costing-lab.css'
 import './styles/cvp-lab.css'
 import './styles/first-move-trainer.css'
 import './styles/core-pattern-trainer.css'
-import './styles/department-lab.css'
 import './styles/cbt-practice.css'
+import './styles/learning-hub.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-    <div className="standalone-lab">
-      <ProcessCostLab />
-      <ProcessMethodLab />
-      <DepartmentAllocationLab />
-      <VarianceLab />
-      <OverheadVarianceLab />
-      <DirectCostingLab />
-      <CvpLab />
-      <FirstMoveTrainer />
-      <CorePatternTrainer />
-      <CbtPractice />
-    </div>
+    <LearningHub />
   </React.StrictMode>,
 )
