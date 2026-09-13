ccf965800706a9caf91483d854fa7765b2bebf7b
butyukunn82 <butyukunn82@gmail.com>
2026-09-13 14:15:04 +0900

    feat: mount direct materials variance lab

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import ProcessCostLab from './features/ProcessCostLab'
+import VarianceLab from './features/VarianceLab'
 import './styles/app.css'
 import './styles/flow-fix.css'
 import './styles/process-lab.css'
+import './styles/variance-lab.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <div className="standalone-lab">
       <ProcessCostLab />
+      <VarianceLab />
     </div>
   </React.StrictMode>,
 )
